26dc70b895b0800980d98f8e95a2c7c6d687a944
Tom Wenger <tom.wenger@lambda-it.ch>
2025-04-13 11:13:04 +0200

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'midi-player';
+
+  // @ViewChild('canvasElement', { static: true })
+  // canvasRef!: ElementRef<HTMLCanvasElement>;
+
+  canvasElement =
+    viewChild.required<ElementRef<HTMLCanvasElement>>('canvasElement');
+
+  private ctx!: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    const canvas = this.canvasElement().nativeElement;
+    this.ctx = canvas.getContext('2d')!;
+
+    // Example: draw a red rectangle
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(50, 50, 100, 100);
+  }
 }
